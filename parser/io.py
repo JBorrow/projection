69567eec40be94d378458ff7c95eca0a118d518d
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-01-07 10:59:15 +0000

    Added input/output routines for the database to/from the python objects for Collectors and Secitons.

--- parser/io.py
+++ parser/io.py
@@ -1,4 +1,5 @@
 import sqlite3
+from .generators import Collector, Section
 
 class Database(object):
     """
@@ -26,7 +27,6 @@ class Database(object):
         Create the tables in the sqlite3 database.
         """
         
-
         c = self.conn.cursor()
 
         c.execute("""
@@ -48,5 +48,90 @@ class Database(object):
 
         return
 
+    
+    def insert_collectors(self, collectors):
+        """
+        Insert an item into the collectors table.
+        """
 
+        c = self.conn.cursor()
+
+        c.execute("insert into collectors values (?,?,?,?,?,?,?,?)", collector)
+
+        self.conn.commit()
+
+        c.close()
+
+        return
+
+
+    def insert_sections(self, section):
+        """
+        Insert an item into the sections table.
+        """
+
+        c = self.conn.cursor()
+
+        c.execute("insert into sections values (?,?,?,?,?,?,?,?,?)", section)
+
+        self.conn.commit()
+
+        c.close()
+
+        return
+
+    
+    def grab_collectors(self):
+        """
+        Grab a list of all collectors as collector objects.
+        """
+
+        c = self.conn.cursor()
+
+        db_output = c.execute("select * from collectors")
+
+        c.close()
+
+        keys = [
+            "input",
+            "line",
+            "regex",
+            "uid",
+            "text",
+            "temporary_replacement",
+            "output_text",
+            "id",
+        ]
+
+        collectors = [Collector(dict(zip(keys, values))) for values in db_output]
+
+        return collectors
+
+
+    def grab_sections(self):
+        """
+        Grab a list of all sections as section objects.
+        """
+
+        c = self.conn.cursor()
+
+        db_output = c.execute("select * from sections")
+
+        c.close()
+
+        keys = [
+            "input",
+            "regex",
+            "uid",
+            "text",
+            "temporary_replacement",
+            "output_text",
+            "startline",
+            "endline",
+            "id",
+        ]
+
+        sections = [Section(dict(zip(keys, values))) for values in db_output]
+
+        return sections
 
