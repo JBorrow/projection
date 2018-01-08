4063fe9e7bde7187c5a3600a25e00c8a5b419687
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-01-08 12:23:27 +0000

    Added a markdown output for the whole text

--- example/example.py
+++ example/example.py
@@ -4,10 +4,13 @@ import os
 
 os.remove("example.db")
 
-Config(
+config = Config(
     tex_filename="example.tex",
     config_filename="config.yml"
 )
+
+with open("example.md", "w") as file:
+    file.write("\n".join(config.parser.text))
 
 exit(0)
 
